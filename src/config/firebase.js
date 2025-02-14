95c0b199b589f218ad91bdc86a37c63b0b1d0505
martin aguilera <martin.aguilera0233@gmail.com>
2025-02-14 11:07:06 -0300

    hol

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
-	apiKey:process.env.REACT_APP_apiKey,
+	apiKey: process.env.REACT_APP_apiKey,
 	authDomain: process.env.REACT_APP_authDomain,
 	projectId: process.env.REACT_APP_projectId,
 	storageBucket: process.env.REACT_APP_storageBucket,
 	messagingSenderId: process.env.REACT_APP_messagingSenderId,
 	appId: process.env.REACT_APP_appId,
 };
-
+console.log("API KEY:", firebaseConfig.apiKey);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
